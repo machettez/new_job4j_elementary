32d7e39d9476f9db944baf2eaf4a016990424d85
Никита <nightwaymoon@gmail.com>
2024-09-15 02:55:31 +0300

    4.2. Заголовок в файле java [#159036 #335820]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("15.09.2024");
+        System.out.println("15.09.2024!");
     }
 }
